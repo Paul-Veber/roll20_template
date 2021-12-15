f6f98a56b49bea3e48f9bbfb0d3c4327b01e8a97
Paul Veber <paulveber56@gmail.com>
2021-12-15 10:02:01 +0100

    adding taillwind for prototyping. test theming the sheet

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,74 @@
 extern crate env_logger;
 extern crate handlebars;
-extern crate serde;
 extern crate serde_derive;
 extern crate serde_json;
-use serde_json::value::{ Map, Value as Json};
 
-use std::io::Result;
+use serde_json::value::{Map, Value as Json};
+
+use std::io::Result as NoErroResult;
 use std::fs::File;
+use std::fs::read_to_string;
 use std::io::Write;
 use std::path::Path;
 
-use handlebars::{
-     Handlebars
-};
+use handlebars::{Helper,Handlebars, Context, RenderContext, Output, RenderError, JsonRender};
+
+pub fn deserialized_json_file(path: &str) -> NoErroResult<Map<String, Json>> {
+    let base_path: String = "./src/template/".to_owned();
+    let full_path = base_path + path;
+
+    let file_path = Path::new(&full_path);
+    let json_file = File::open(file_path).expect("file not found");
+
+    let deserialized_file: Map<String, Json> =
+        serde_json::from_reader(json_file).expect("error reading file");
+
+    Ok(deserialized_file)
+}
 
 // fetch data from variable.json
-pub fn handle_variables() -> Map<String, Json>  {
+pub fn handle_variables() -> Map<String, Json> {
     let file_path = Path::new("./src/template/variables.json");
     let json_file = File::open(file_path).expect("file not found");
-    let deserialized_file: Map<String, Json> = serde_json::from_reader(json_file).expect("error reading file");
+    let deserialized_file: Map<String, Json> =
+        serde_json::from_reader(json_file).expect("error reading file");
 
     deserialized_file
 }
 
-pub fn handle_templating() -> Result<()> {
+pub fn handle_templating() -> NoErroResult<()> {
+    let base_path = "./src/template/";
+    let handlebars = Handlebars::new();
 
-    let mut handlebars = Handlebars::new();
+    let data = deserialized_json_file("variables.json")?;
 
-    let data = handle_variables();
+    let partials_json = deserialized_json_file("partials.json").unwrap();
+    let partials_path_array = partials_json["partials"].as_array().unwrap();
+    //fetch partials content
+    let partials_array: Vec<String> = partials_path_array.iter()
+    .map(|x| read_to_string(base_path.to_owned() + x.as_str().expect("value from partials.json can't be read"))
+    .expect("partials in partials.json is not an array"))
+    .collect();
 
-    handlebars.register_template_file("template", "./src/template/template.hbs").unwrap();
+    //merge all partials content
+    let partials: String = partials_array.iter()
+    .flat_map(|s| s.chars())
+    .collect();
+
+    let template = read_to_string(base_path.to_owned() + "template.hbs").expect("error template. check if syntaxe is valid");
+    let full_template = partials + template.clone().as_mut_str(); 
 
     let mut output_file = File::create("./target/sheet.html")?;
+    handlebars
+        .render_template_to_write(&full_template, &data, &mut output_file)
+        .expect("failed to write file");
 
-    handlebars.render_to_write("template", &data, &mut output_file).expect("failed to write file");
     println!("target/table.html generated");
 
     Ok(())
 }
 
-pub fn handle_scss() -> Result<()> {
-
+pub fn handle_scss() -> NoErroResult<()> {
     let sass = grass::from_path("./src/css/input.scss", &grass::Options::default()).unwrap();
 
     let mut css_file = File::create("./target/styles.css")?;
@@ -49,14 +77,12 @@ pub fn handle_scss() -> Result<()> {
     Ok(())
 }
 
-
-fn main()-> Result<()> {
-    
+fn main() -> NoErroResult<()> {
     env_logger::init();
 
     handle_templating()?;
 
-    handle_scss()?;
+    //handle_scss()?;
 
     Ok(())
 }
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  content: ["./src/**/*.hbs"],
+  theme: {
+      colors: {
+          'flamingo': '#ECBFBD',
+          'magenta': '#C6AAE8',
+          'pink': '#F0AFE1',
+          'red': '#E28C8C',
+          'peach': '#F7C196',
+          'yellow': '#EADDA0',
+          'green': '#B3E1A3',
+          'blue': '#A4B9EF',
+          'black1': '#15121C',
+          'black2': '#1B1923',
+          'black3': '#1E1E28',
+          'black4': '#2D293B',
+          'black5': '#3E4058',
+          'gray': '#6E6C7C',
+          'white': '#D7DAE0',
+          'teal': '#9DDDCB'
+      },
+    extend: {},
+  },
+  plugins: [],
+}
